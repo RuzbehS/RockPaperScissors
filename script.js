4cb9f778000ee594087428a1bdd6455c4f66c74c
RuzbehS <ruzbeh.schafaie@mailbox.org>
2021-05-24 12:45:12 +0200

    added conditions and pseudocode to scrpt.js

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 /*
-PSEUDOCODE:
 
+Conditions:
+game against computer
+game should be able to run several times in a row and keep score
+at the end report the winner, but also the results of each round
+
+PSEUDOCODE:
+initialize string variables for rock/paper/scissors
+initialize variables that keep track of scores for both players
+initialize starting message and ask user for input
+take input from the user as either a string or a number corresponding to the available options rock/paper/scissors
+save input in a variable userVar
+let computer randomly roll an option
+save to a variable comVar
+compare userVar against comVar to determine winner
+update scores
+repeat game 5 times
+compare score variables between both players to determine winner
+print the winner and individual scores
+
+
+call a function for the computer 
 */
+console.log("Wut")
