3c7cac459a00f3d9eb99b10b0c2c49f408cc2fac
RuzbehS <ruzbeh.schafaie@mailbox.org>
2021-05-24 15:47:25 +0200

    added loop to repeat games five times

--- script.js
+++ script.js
@@ -6,12 +6,11 @@ game should be able to run several times in a row and keep score
 at the end report the winner, but also the results of each round
 
 PSEUDOCODE:
-initialize string variables for rock/paper/scissors
 initialize variables that keep track of scores for both players
 initialize starting message and ask user for input
 take input from the user as either a string or a number corresponding to the available options rock/paper/scissors
 save input in a variable userVar
-let computer randomly roll an option
+let computer randomly roll an option and save as a variable comVar
 save to a variable comVar
 compare userVar against comVar to determine winner
 update scores
@@ -23,15 +22,14 @@ print the winner and individual scores
 call a function for the computer 
 */
 
-var rock = 1;
-var paper = 2;
-var scissors = 3;
 var userScore = 0;
 var comScore = 0; 
-var winner = "";
-var playerOption = playerSelection();
-var comOption = getRndInteger(1,3);
-determineWinner();
+
+for (i = 0; i < 5; i++) {
+    var playerOption = playerSelection();
+    var comOption = getRndInteger(1,3);
+    determineWinner();
+}
 
 function determineWinner() {
     if (playerOption == 1 && comOption == 3) {
